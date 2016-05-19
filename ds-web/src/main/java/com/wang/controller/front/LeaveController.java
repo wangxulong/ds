1d163e973e40b9d17698790e1914b2b4d1307971
wangxulong <duanxian0402@126.com>
2016-05-19 22:01:44 +0800

    请假管理

--- ds-web/src/main/java/com/wang/controller/front/LeaveController.java
+++ ds-web/src/main/java/com/wang/controller/front/LeaveController.java
@@ -1,22 +1,43 @@
 package com.wang.controller.front;
 
+import com.wang.controller.front.FrontController;
+import com.wang.entity.TLeave;
+import com.wang.entity.TStudent;
+import com.wang.service.LeaveService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
-/**
- * Created by wxl on 2016/5/11.
- */
 @Controller
 @RequestMapping("/front/leave/")
-public class LeaveController {
+public class LeaveController extends FrontController {
+    @Resource
+    private LeaveService leaveService;
+
+    public LeaveController() {
+    }
+
     @RequestMapping("index")
-    public void index(){
+    public void index(Model model, HttpSession session) {
+        TStudent loginStudent = this.getLoginUser(session);
+        model.addAttribute("leaves", this.leaveService.getMyLeaves(loginStudent.getId()));
     }
 
-    @RequestMapping(value = "submit")
-    public void submit(){
-
+    @RequestMapping(value = "submit",method = RequestMethod.GET)
+    public void submit() {
     }
 
-
+    @RequestMapping(
+            value = "submit",
+            method = RequestMethod.POST
+    )
+    public String doPost(MultipartFile file, TLeave leave) {
+        System.out.println("dadfsd");
+        return "redirect:index";
+    }
 }
+
